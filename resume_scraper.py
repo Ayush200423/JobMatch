514e8081a91a861c2ce8be7b155beb74f1904120
Ayush Patel <ayush200423@gmail.com>
2022-08-11 19:35:14 -0400

    modified return type

--- resume_scraper.py
+++ resume_scraper.py
@@ -2,16 +2,42 @@ import spacy
 from PyPDF2 import PdfReader
 
 class ResumeParser:
-    def __init__(self, file_path):
+    def __init__(self):
         self.nlp = spacy.load('./resumeparser/output/model-best')
-        self.file = file_path
+        self.ents_dict = {
+            'name': [],
+            'fname': [],
+            'lname': [],
+            'email address': [],
+            'phone': [],
+            'college name': [],
+            'degree': [],
+            'graduation year': [],
+            'years of experience': [],
+            'company': [],
+            'role': [],
+            'skills': [],
+            'location': []
+        }
     
-    def pdf_to_txt(self):
-        reader = PdfReader(self.file)
+    def pdf_to_txt(self, file_path):
+        reader = PdfReader(file_path)
         page = reader.pages[0]
         self.text = page.extract_text()
     
     def find_ents(self):
         doc = self.nlp(self.text)
         for ent in doc.ents:
-            print(ent.text, ent.label_)
+            label = ent.label_
+            if str(ent) in self.ents_dict[label.lower()]:
+                continue
+            else:
+                self.ents_dict[label.lower()].append(str(ent))
+        try:
+            self.ents_dict['fname'] = self.ents_dict['name'][0].split()[0]
+            self.ents_dict['lname'] = self.ents_dict['name'][0].split()[1]
+        except:
+            self.ents_dict['fname'] = self.ents_dict['name']
+        else:
+            self.ents_dict.pop('name')
+        return self.ents_dict
